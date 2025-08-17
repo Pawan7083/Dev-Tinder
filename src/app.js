f0744ccad6c5dd6b1f9f396713fdeda42d1ac68e
Pawan Kumar <pawankr7083@gmail.com>
2025-08-17 22:59:40 +0530

    install express and create a server using this

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
-const http= require("http");
+const express = require("express");
 
-const app= http.createServer((req,res)=>{
-    console.log("this is server ");
-    res.end("Hello server");
-});
+const  app= express();
+app.get("/user",(req,res,next)=>{
+    console.log("This is first /user request handler.");
+    next();
+})
+app.get("/user",(req,res,next)=>{
+    console.log("This is second /user request handler.");
+    // res.send("This is the 2nd /user request handler.")
+    next();
+})
+app.get("/user",(req,res)=>{
+    const {name,id}=req.query;
+    res.end("<h1>This is the 3rd /user request handler.<h1>"+name + " "+ id);
+})
+
+app.get("/user/:name/:id",(req,res,next)=>{
+    const {name,id}=req.params;
+    res.status(201).end(name + " "+ id);
+})
+
 
 
 app.listen(7777);
